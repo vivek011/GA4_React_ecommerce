105652f10900f01ef5d937016dccf536a98599ec
“Vivek18.Gupta” <Vivek18.Gupta@ril.com>
2023-06-14 17:35:34 +0530

    Updates

---
+++ precache-manifest.cdeed9f0c78110d9ba1304a582ce0ae2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "55b7e3eb6aabb0dbf334",
+    "url": "/static/css/main.294c0d0d.chunk.css"
+  },
+  {
+    "revision": "55b7e3eb6aabb0dbf334",
+    "url": "/static/js/main.5f1f2d48.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c44af2f97180d850d7d6",
+    "url": "/static/js/2.bc4e413d.chunk.js"
+  },
+  {
+    "revision": "4290bc807f898ade8bc9a4296735ab10",
+    "url": "/static/media/newLogo.4290bc80.svg"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/static/media/shopping-bag.09e6f5e7.svg"
+  },
+  {
+    "revision": "5528aaeefb94f6fe16650e707cdadf77",
+    "url": "/index.html"
+  }
+];
