8ffb1502264c4d7118a10d9285f8b5b8622d7f47
“Vivek18.Gupta” <Vivek18.Gupta@ril.com>
2023-06-14 17:56:57 +0530

    Updates

---
+++ precache-manifest.5b8826c41e3d8b19c3b239b8e4a84285.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "55b7e3eb6aabb0dbf334",
+    "url": "/GA4_React_ecommerce/static/css/main.294c0d0d.chunk.css"
+  },
+  {
+    "revision": "55b7e3eb6aabb0dbf334",
+    "url": "/GA4_React_ecommerce/static/js/main.5f1f2d48.chunk.js"
+  },
+  {
+    "revision": "c9f545d2042d582d41fe",
+    "url": "/GA4_React_ecommerce/static/js/runtime~main.9b2498d8.js"
+  },
+  {
+    "revision": "8971c46fa480891bdb6b",
+    "url": "/GA4_React_ecommerce/static/js/2.ea469100.chunk.js"
+  },
+  {
+    "revision": "4290bc807f898ade8bc9a4296735ab10",
+    "url": "/GA4_React_ecommerce/static/media/newLogo.4290bc80.svg"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/GA4_React_ecommerce/static/media/shopping-bag.09e6f5e7.svg"
+  },
+  {
+    "revision": "1379b395c614c5a48f68189f87f780a1",
+    "url": "/GA4_React_ecommerce/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cdeed9f0c78110d9ba1304a582ce0ae2.js"
+  "/GA4_React_ecommerce/precache-manifest.5b8826c41e3d8b19c3b239b8e4a84285.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/GA4_React_ecommerce/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
