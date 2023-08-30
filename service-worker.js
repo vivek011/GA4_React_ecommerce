e74611a34e85a8cd2161334db42c18ccf84f4606
“Vivek18.Gupta” <Vivek18.Gupta@ril.com>
2023-08-30 13:39:43 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/GA4_React_ecommerce/precache-manifest.119eaaf1dbcf0516895affedce184f18.js"
+  "/GA4_React_ecommerce/precache-manifest.3042d7753a991ee3b393d5fabccb9512.js"
 );
 
 workbox.clientsClaim();
